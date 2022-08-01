7bc39343b8a58a4c5b0ff2f413b1ec4e9a27b119
smz6990 <saleh.mohammadzadeh@gmail.com>
2022-08-01 19:05:37 +0430

    comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from blog.models import Category,Post
+from blog.models import Category,Post,Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('id','title','author','status','counted_views','published_date','created_date')
+    list_display = ('title','id','author','status','counted_views','published_date','created_date')
     list_filter = ('status','author')
     search_fields = ('title','content')
     
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name','post','email','approved','created_date')
+    list_filter = ('post','approved')
+    search_fields = ('post','name','email')
 
 admin.site.register(Category)
     
