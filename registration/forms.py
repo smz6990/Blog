ea9d4c046af23e22b393a0c00f15be84ec798d15
smz6990 <saleh.mohammadzadeh@gmail.com>
2022-08-02 21:32:28 +0430

    authentication is done

--- registration/forms.py
+++ registration/forms.py
@@ -1,9 +1,34 @@
 from django.contrib.auth.forms import AuthenticationForm
 from simplemathcaptcha.fields import MathCaptchaField
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 class UserAuthenticationForm(AuthenticationForm):
     captcha = MathCaptchaField()
     class Meta:
         model = User
         fields = ['username','password','captcha']
+        
+        
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=255,required=False)
+    last_name = forms.CharField(max_length=255,required=False)
+    captcha = MathCaptchaField()
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2','first_name','last_name','captcha']
+    
+    def save(self,commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        
+        if self.cleaned_data['first_name']:
+            user.first_name = self.cleaned_data['first_name']
+        if self.cleaned_data['last_name']:
+            user.last_name = self.cleaned_data['last_name']
+        
+        if commit:
+            user.save()
+        return user   
--- registration/views.py
+++ registration/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import AuthenticationForm
-from registration.forms import UserAuthenticationForm
+from registration.forms import UserAuthenticationForm , CustomUserCreationForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signin_view(request):
-    next = request.POST.get('next','/')
     if not request.user.is_authenticated:
         if request.method == 'POST':
             form = UserAuthenticationForm(request=request,data=request.POST)
@@ -17,20 +17,72 @@ def signin_view(request):
                 if user is not None:
                     login(request,user)
                     messages.info(request,'Logged in successfully')
-                    messages.info(request,f'Welcome {username}')
+                    messages.info(request,f'Welcome , {username}')
                 else:
                     messages.error(request,'Username or Password is wrong!')
             else:
                 messages.error(request,'Something went wrong')
                 messages.error(request,form.errors)
             
-    return redirect(next)
+            next = request.POST.get('next','/')
+            return redirect(next)
+        
+        else: #request.method == 'GET'
+            form = UserAuthenticationForm()
+            next = request.GET.get('next','/')
+            context = {'form':form,'next':next}
+            
+            return render(request,'registration/signin.html',context)
     
+    next = request.GET.get('next','/')
+    return redirect(next)
+
+
+@login_required(redirect_field_name='next',login_url='/registration/signin/',)
 def signout_view(request):
-    return
+    next = request.GET.get('next','/')
+    logout(request)
+    return redirect(next)
 
 def signup_view(request):
-    return
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = CustomUserCreationForm(request.POST)
+            if form.is_valid():
+                username = request.POST['username']
+                email = request.POST['email']
+                try:
+                    username = User.objects.get(username=username)
+                except:
+                    pass
+                else:
+                    messages.error(request,'Username is taken')
+                    form = CustomUserCreationForm()
+                    next = request.POST.get('next','/')
+                    return  render(request,'registration/signup.html',{'form':form,'next':next})
+                try:
+                    email = User.objects.get(email=email)
+                except:
+                    user = form.save()
+                    login(request,user)
+                    messages.success(request,'Account created successfully')
+                    messages.success(request,f'Welcome , {user.username}')
+                    next = request.GET.get('next','/')
+                    return redirect(next)
+                else:               
+                    messages.error(request,'Email is taken')
+            else:
+                messages.error(request,'Something went wrong')
+                messages.error(request,form.errors)
+                
+            next = request.POST.get('next','/')
+        else:
+            next = request.GET.get('next','/')
+        form = CustomUserCreationForm()
+        return  render(request,'registration/signup.html',{'form':form,'next':next})
+    
+    next = request.GET.get('next','/')
+    return redirect(next)
 
 def reset_view(request):
     return
